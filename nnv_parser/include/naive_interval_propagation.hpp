7bc6643115e5d3a5d7abc1830e97d0c6fc6f3056
ytsao <tasocc456@gmail.com>
2025-10-24 11:25:14 +0200

    [ADD] the implementation of naive interval propagation is done.

--- nnv_parser/include/naive_interval_propagation.hpp
+++ nnv_parser/include/naive_interval_propagation.hpp
@@ -9,32 +9,43 @@ class NaiveIntervalPropagation : public Propagation {
 public:
   IntervalDomain a;
 
-  bool execute(Network nnv) override {
+  bool execute(Network &nnv) override {
     std::cout << "Executing Naive Interval Propagation..." << std::endl;
 
-    for (size_t layer_idx = 0; layer_idx < nnv.layers.size() - 1; ++layer_idx) {
+    for (size_t layer_idx = 0; layer_idx < nnv.layers.size(); ++layer_idx) {
+      std::cout << "Processing Layer " << layer_idx << " ... " << std::endl;
       if (nnv.layers[layer_idx].type == LayerType::Sub) {
-        a.subtraction_layer_transformer(nnv.layers[layer_idx],
-                                        nnv.layers[layer_idx + 1]);
+        a.subtraction_layer_transformer(nnv.layers[layer_idx]);
       } else if (nnv.layers[layer_idx].type == LayerType::Flatten) {
-        a.flatten_layer_transformer(nnv.layers[layer_idx],
-                                    nnv.layers[layer_idx + 1]);
+        std::cout << "starting flatten layer transformation\n";
+        a.flatten_layer_transformer(nnv.layers[layer_idx]);
+        std::cout << "finished flatten layer transformation\n";
       } else if (nnv.layers[layer_idx].type == LayerType::Relu) {
-        a.relu_layer_transformer(nnv.layers[layer_idx],
-                                 nnv.layers[layer_idx + 1]);
+        std::cout << "starting relu layer transformation\n";
+        a.relu_layer_transformer(nnv.layers[layer_idx - 1],
+                                 nnv.layers[layer_idx]);
+        std::cout << "finished relu layer transformation\n";
       } else if (nnv.layers[layer_idx].type == LayerType::MatMul) {
-        a.matmul_layer_transformer(nnv.layers[layer_idx],
-                                   nnv.layers[layer_idx + 1]);
+        std::cout << "starting matmul layer transformation\n";
+        a.matmul_layer_transformer(nnv.layers[layer_idx - 1],
+                                   nnv.layers[layer_idx]);
+        std::cout << "finished matmul layer transformation\n";
       } else if (nnv.layers[layer_idx].type == LayerType::Add) {
-        a.add_layer_transformer(nnv.layers[layer_idx],
-                                nnv.layers[layer_idx + 1]);
+        std::cout << "starting add layer transformation\n";
+        a.add_layer_transformer(nnv.layers[layer_idx - 1],
+                                nnv.layers[layer_idx]);
+        std::cout << "finished add layer transformation\n";
       } else if (nnv.layers[layer_idx].type == LayerType::Gemm) {
-        a.gemm_layer_transformer(nnv.layers[layer_idx],
-                                 nnv.layers[layer_idx + 1]);
+        std::cout << "starting gemm layer transformation\n";
+        a.gemm_layer_transformer(nnv.layers[layer_idx - 1],
+                                 nnv.layers[layer_idx]);
+        std::cout << "finished gemm layer transformation\n";
+      } else {
+        std::cout << "Unknown layer type!" << std::endl;
       }
     }
 
-    return false;
+    return true;
   }
 };
 

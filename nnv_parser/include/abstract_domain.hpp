0495d118839ce10614e4f5a70d88b55796e0b8fe
ytsao <tasocc456@gmail.com>
2025-10-26 19:06:12 +0100

    [ADD] new flatten layer function for creating symbolic expressions

--- nnv_parser/include/abstract_domain.hpp
+++ nnv_parser/include/abstract_domain.hpp
@@ -1,15 +1,26 @@
 #ifndef ABSTRACT_DOMAIN_HPP
 #define ABSTRACT_DOMAIN_HPP
 
-#include "layer.hpp"
-#include "specifications.hpp"
+// #include "layer.hpp"
+#include "network.hpp"
 
 class AbstractDomain {
 public:
   virtual ~AbstractDomain() = default;
 
-  virtual void subtraction_layer_transformer(const Layer &current_layer) = 0;
-  virtual void flatten_layer_transformer(const Layer &current_layer) = 0;
+  /*
+   * It is the concretization function that maps the abstract element to the
+   * interval bounds by its dependency layer in the network.
+   * */
+  virtual void gamma(Network &nnv, const size_t layer_idx) { return; }
+
+  virtual void subtraction_layer_transformer(const Layer &current_layer) {
+    return;
+  };
+  virtual void flatten_layer_transformer(const Layer &current_layer) {
+    return;
+  };
+  virtual void flatten_layer_transformer(Layer &current_layer) { return; };
   virtual void relu_layer_transformer(const Layer &from_layer,
                                       Layer &to_layer) = 0;
   virtual void matmul_layer_transformer(const Layer &from_layer,

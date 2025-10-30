0521d9639c0be6c3186753704a9556708d5d1d0f
ytsao <tasocc456@gmail.com>
2025-10-30 13:46:16 +0100

    [ADD] create auxiliary (spec) layer for each forward propagation.

--- nnv_parser/include/propagation.hpp
+++ nnv_parser/include/propagation.hpp
@@ -8,6 +8,94 @@ public:
   virtual ~Propagation() = default;
 
   virtual bool execute(Network &nnv) = 0;
+
+protected:
+  void create_auxiliary_layer(Network &nnv) {
+    std::cout << "  Creating auxiliary layer for postconditions..."
+              << std::endl;
+
+    Layer auxiliary_layer;
+    auxiliary_layer.type = LayerType::Gemm;
+
+    if (nnv.spec.A.empty()) {
+      // For prop_1.vnnlib
+      std::cout << "  No postconditions specified. Skipping auxiliary layer."
+                << std::endl;
+      return;
+    }
+
+    std::cout << "  Number of OR conditions: " << nnv.spec.A.size()
+              << std::endl;
+    std::cout << "  Number of AND conditions per OR condition: "
+              << nnv.spec.A[0].size() << std::endl;
+    auxiliary_layer.layer_size =
+        nnv.spec.A.size() *
+        nnv.spec.A[0]
+            .size(); // number of OR conditions * number of AND conditions.
+
+    std::cout << "  Auxiliary layer size: " << auxiliary_layer.layer_size
+              << std::endl;
+
+    // Initialize the size of weights matrix.
+    std::vector<float> zero_weights(nnv.output_size, 0.0f);
+    for (size_t j = 0; j < auxiliary_layer.layer_size; ++j) {
+      auxiliary_layer.weights.push_back(zero_weights);
+    }
+
+    // Initialize deeppoly expressions for each neuron in the auxiliary_layer
+    auxiliary_layer.deeppoly_lower_expressions =
+        std::vector<std::vector<double>>(
+            auxiliary_layer.layer_size,
+            std::vector<double>(nnv.layers.back().layer_size, 0.0));
+    auxiliary_layer.deeppoly_upper_expressions =
+        std::vector<std::vector<double>>(
+            auxiliary_layer.layer_size,
+            std::vector<double>(nnv.layers.back().layer_size, 0.0));
+
+    // Create a set of new neurons and its lower & upper biases.
+    for (size_t i = 0; i < auxiliary_layer.layer_size; ++i) {
+      Neuron neuron;
+      neuron.setId(i);
+      neuron.setLayerId(nnv.layers.size());
+      auxiliary_layer.neurons.push_back(neuron);
+      auxiliary_layer.biases.push_back(0.0f);
+      auxiliary_layer.lower_biases.push_back(0.0f);
+      auxiliary_layer.upper_biases.push_back(0.0f);
+    }
+
+    for (size_t i = 0; i < nnv.spec.A.size(); ++i) {
+      for (size_t j = 0; j < nnv.spec.A[i].size(); ++j) {
+        std::cout << "i: " << i << ", j: " << j << std::endl;
+        char relation = nnv.spec.relations[i][j];
+        for (size_t k = 0; k < nnv.spec.A[i][j].size(); ++k) {
+          double coef = nnv.spec.A[i][j][k];
+          std::cout << "Coef: " << coef << " for neuron index "
+                    << (i * nnv.spec.A[i].size() + j) << " and output index "
+                    << k << std::endl;
+
+          auxiliary_layer.weights[i * nnv.spec.A[i].size() + j][k] = coef;
+          // auxiliary_layer
+          //     .deeppoly_lower_expressions[i * nnv.spec.A[i].size() + j][k] =
+          //     coef;
+          // auxiliary_layer
+          //     .deeppoly_upper_expressions[i * nnv.spec.A[i].size() + j][k] =
+          //     coef;
+        }
+      }
+    }
+
+    nnv.layers.push_back(auxiliary_layer);
+
+    // print weights of the auxiliary layer
+    for (auto weights_row : auxiliary_layer.weights) {
+      for (auto weight : weights_row) {
+        std::cout << weight << ", ";
+      }
+      std::cout << std::endl;
+    }
+
+    std::cout << "Finished creating auxiliary layer." << std::endl;
+  }
 };
 
 #endif // PROPAGATION_HPP

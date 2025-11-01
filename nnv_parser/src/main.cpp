10ad3041f12151c6bf86c8368f8b3706e933a721
ytsao <tasocc456@gmail.com>
2025-11-01 15:39:30 +0100

    [MODIFIED] it is now able to use a csv file to do batch verification or a command to do single verification instance.

--- nnv_parser/src/main.cpp
+++ nnv_parser/src/main.cpp
@@ -5,80 +5,81 @@
 #include "utility.hpp"
 #include <iostream>
 
+void verify(const std::string &vnnlib_path, const std::string &onnx_path,
+            const std::string &propagation_method, size_t &num_verified) {
+  // create network object and parse vnnlib & onnx file.
+  Network nnv;
+
+  std::cout << "Parsing vnnlib: " << vnnlib_path << " ..." << std::endl;
+  if (!nnv.read_vnnlib(vnnlib_path)) {
+    std::cerr << "Error: Could not parse vnnlib file " << vnnlib_path
+              << std::endl;
+    return;
+  }
+  std::cout << "Parsing vnnlib is done." << std::endl;
+  std::cout << "Parsing onnx: " << onnx_path << " ..." << std::endl;
+  if (!nnv.read_onnx(onnx_path)) {
+    std::cerr << "Error: Could not parse onnx file " << onnx_path << std::endl;
+    return;
+  }
+  std::cout << "Parsing onnx is done." << std::endl;
+
+  // Execute the propagation method.
+  if (std::strcmp(propagation_method.c_str(), "ibp") == 0) {
+    NaiveIntervalPropagation ibp;
+    if (ibp.execute(nnv)) {
+      std::cout << "The naive_interval_propagation result is UNSAT."
+                << std::endl;
+      num_verified++;
+    } else {
+      std::cerr << "The naive_interval_propagation is not able to have "
+                   "a conclusive verification result."
+                << std::endl;
+    }
+  } else if (std::strcmp(propagation_method.c_str(), "sip") == 0) {
+    SymbolicIntervalPropagation sip;
+    if (sip.execute(nnv)) {
+      std::cout << "The symbolic_interval_propagation result is UNSAT."
+                << std::endl;
+      num_verified++;
+    } else {
+      std::cerr << "The symbolic_interval_propagation is not able to have a "
+                   "conclusive verification result."
+                << std::endl;
+    }
+  } else if (std::strcmp(propagation_method.c_str(), "deeppoly") == 0) {
+    DeepPolyPropagation deeppoly;
+    if (deeppoly.execute(nnv)) {
+      std::cout << "The deeppoly_propagation result is UNSAT." << std::endl;
+      num_verified++;
+    } else {
+      std::cerr << "The deeppoly_propagation is not able to have a conlcusive "
+                   "verification result."
+                << std::endl;
+    }
+  }
+  // nnv.dump_all_bounds();
+  nnv.dump_bounds_at_layer(nnv.layers.size() - 2);
+  nnv.dump_bounds_at_layer(nnv.layers.size() - 1);
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     std::cout << "usage: " << argv[0] << " <input_file>" << std::endl;
   }
 
-  std::vector<Utility::TaskEntry> tasks = Utility::read_csv(argv[1]);
-  std::cout << "Number of tasks = " << tasks.size() << std::endl;
-  for (Utility::TaskEntry task : tasks) {
-    std::cout << "VNNLIB: " << task.vnnlib_path << ", ONNX: " << task.onnx_path
-              << ", METHOD: " << task.propagation_method << std::endl;
-    std::ifstream f(task.vnnlib_path);
-    if (!f.is_open()) {
-      std::cerr << "Error: Could not open file " << task.vnnlib_path
-                << std::endl;
-      return 1;
+  size_t num_verified = 0;
+  if (argc == 2) {
+    std::vector<Utility::TaskEntry> tasks = Utility::read_csv(argv[1]);
+    for (Utility::TaskEntry task : tasks) {
+      verify(task.vnnlib_path, task.onnx_path, task.propagation_method,
+             num_verified);
+      std::cout << "Number of verified: " << num_verified << std::endl;
     }
-    std::ostringstream buffer;
-    buffer << f.rdbuf();
-    std::string input = buffer.str();
-    f.close();
-
-    // create network object and parse vnnlib & onnx file.
-    Network nnv;
-
-    std::cout << "Parsing vnnlib: " << task.vnnlib_path << " ..." << std::endl;
-    if (!nnv.read_vnnlib(task.vnnlib_path)) {
-      std::cerr << "Error: Could not parse vnnlib file " << task.vnnlib_path
-                << std::endl;
-      return 1;
-    }
-    std::cout << "Parsing vnnlib is done." << std::endl;
-    std::cout << "Parsing onnx: " << task.onnx_path << " ..." << std::endl;
-    if (!nnv.read_onnx(task.onnx_path)) {
-      std::cerr << "Error: Could not parse onnx file " << task.onnx_path
-                << std::endl;
-      return 1;
-    }
-    std::cout << "Parsing onnx is done." << std::endl;
-
-    // Execute the propagation method.
-    if (std::strcmp(task.propagation_method.c_str(), "ibp") == 0) {
-      NaiveIntervalPropagation ibp;
-      if (ibp.execute(nnv)) {
-        std::cout << "The naive_interval_propagation result is UNSAT."
-                  << std::endl;
-      } else {
-        std::cerr << "The naive_interval_propagation is not able to have "
-                     "a conclusive verification result."
-                  << std::endl;
-      }
-    } else if (std::strcmp(task.propagation_method.c_str(), "sip") == 0) {
-      SymbolicIntervalPropagation sip;
-      if (sip.execute(nnv)) {
-        std::cout << "The symbolic_interval_propagation result is UNSAT."
-                  << std::endl;
-      } else {
-        std::cerr << "The symbolic_interval_propagation is not able to have a "
-                     "conclusive verification result."
-                  << std::endl;
-      }
-    } else if (std::strcmp(task.propagation_method.c_str(), "deeppoly") == 0) {
-      DeepPolyPropagation deeppoly;
-      if (deeppoly.execute(nnv)) {
-        std::cout << "The deeppoly_propagation result is UNSAT." << std::endl;
-      } else {
-        std::cerr
-            << "The deeppoly_propagation is not able to have a conlcusive "
-               "verification result."
-            << std::endl;
-      }
-    }
-    // nnv.print_all_bounds();
-    // nnv.print_bound_at_layer(nnv.layers.size() - 2);
-    nnv.print_bound_at_layer(nnv.layers.size() - 1);
+  } else if (argc == 4) {
+    verify(argv[1], argv[2], argv[3], num_verified);
+  } else {
+    std::cerr << "Error: unknown configuration." << std::endl;
   }
 
   return 0;
